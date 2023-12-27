84a01d93896dc314241f885b3ae8c0d1382e1cd0
Marconi Soares de Jesus <marconi.developer@outlook.com.br>
2023-12-26 22:49:10 -0300

    feat: add index template modeules

--- webpage/portifolio/views.py
+++ webpage/portifolio/views.py
@@ -10,7 +10,19 @@ def about_me(request):
 def index(request):
     TEMPLATE_NAME = "index.html"
     context = {
-        "profile": Profile.objects.prefetch_related("links").first()
+        "profile": Profile.objects.prefetch_related("links").first(),
+        "projects": [{
+            "name":"BackLess",
+            "short_description": "Um removedor de backgrounds de imagens que utiliza tarefas assíncronas e arquitetura serverless. Projeto criado usando Django, Channels, Redis, Postgre, Google Cloud e AWS.",
+            "active": True,
+            "source": "https://github.com/marconi-dev"
+        },{
+            "name":"Flashcards API",
+            "short_description": "Uma API REST para estudos com repetição espaçada inspirada no anki web. Criada com Django e Postgre, implementa autenticação JWT.",
+            "status": "disable",
+            "has_detail": True,
+            "source": "https://github.com/marconi-dev"
+        }]
     }
     
     return render(request, TEMPLATE_NAME, context)
