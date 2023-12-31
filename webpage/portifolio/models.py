863c385f2a5e6bc0e77a6cef0480d421e245130d
Marconi Soares de Jesus <marconi.developer@outlook.com.br>
2023-12-31 17:20:58 -0300

    feat(portifolio/models): add study model

--- webpage/portifolio/models.py
+++ webpage/portifolio/models.py
@@ -126,7 +126,8 @@ class ProjectDetail(ProjectDetailMixin):
 
 ARTICLE_TYPE_CHOICES = PROJECT_TYPE_CHOICES + (
     ('career', 'Carreira'),
-    ('community', 'Comunidade')
+    ('community', 'Comunidade'),
+    ('sdev', 'Desenvolvimento de Software')
 )
 class Article(Model):
     url = models.URLField()
@@ -138,6 +139,33 @@ class Article(Model):
 
     class Meta:
         default_related_name = 'articles'
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return self.title
+
+
+STUDY_SUBJECT_CHOICES = ARTICLE_TYPE_CHOICES
+STUDY_TYPE_CHOICES = (
+    ('vid', 'Video'),
+    ('ppr', 'Papers'),
+    ('art', 'Artigos'),
+    ('blg', 'Blog Post'),
+    ('col', 'Colaborações'),
+    ('tst', 'Testes Pessoais'),
+)
+class Study(Model):
+    url = models.URLField()
+    thought = models.TextField()
+    description = models.TextField()
+    title = models.CharField(max_length=255)
+    technologies = models.ManyToManyField(Technology)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    study_type = models.CharField(choices=STUDY_TYPE_CHOICES)
+    study_subject = models.CharField(choices=STUDY_SUBJECT_CHOICES)
+
+    class Meta:
+        default_related_name = 'studies'
         ordering = ('created_at',)
 
     def __str__(self):
