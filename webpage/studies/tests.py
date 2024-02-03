4f5b63f7ae34bda66204e92caca372ec6bb97e4a
Marconi Soares de Jesus <marconi.developer@outlook.com.br>
2024-02-02 22:49:32 -0300

    add test to ensure latest only retrieves latests studies

--- webpage/studies/tests.py
+++ webpage/studies/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.response import Response
+
+from .models import Study
+
 
 # Create your tests here.
+class StudiesViewSetTestCase(APITestCase):
+    BASE_URL = "studies"
+
+    def setUp(self) -> None:
+        self.base_url = "https://example.com"
+        studies: list[Study] = [
+            Study(url=f"{self.base_url}/{i}") for i in range(0, 4 + 1)
+        ]
+        Study.objects.bulk_create(studies)
+
+    def test_latest_endpoint(self):
+        """
+        Should return the 3 latest studies
+        """
+        url = reverse(f"{self.BASE_URL}-latest")
+        response: Response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(response.data[0].get("url"), f"{self.base_url}/4")
+        self.assertEqual(response.data[1].get("url"), f"{self.base_url}/3")
+        self.assertEqual(response.data[2].get("url"), f"{self.base_url}/2")
